b81cc4383471b7f0113c998e9dd5fd85e33f78f0
Shivanand <Shivanand0297@gmail.com>
2023-03-30 20:41:41 +0530

    axios default configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Title from './components/Title';
+import GlobalInstance from './examples/4-global-instance';
+import "./axiosDefaults/global"
+
+
 function App() {
   return (
     <main>
       <Title />
+      <GlobalInstance/>
+      <CustomInstance/>
     </main>
   );
 }
